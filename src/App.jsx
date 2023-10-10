a9fcdea31b566a778acc5636adefd91f12d0328d
digimori <hanaorikuu@gmail.com>
2023-10-10 13:23:41 +0000

    Add StatefulGreeting - setState added to button and h1 to test.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import css from './App.module.css';
 import FunctionalGreeting from './components/FunctionalGreeting'
 import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
+import StatefulGreeting from './components/StatefulGreeting';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className={css.App}>
         <Sidebar />
         <Navbar />
+        <StatefulGreeting greeting="Yellow!" />
         {/*<FunctionalGreeting greeting="Heeeeey" name="Cee" age="31" />*/}
 
       </div>
