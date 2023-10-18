9843a40c632dbd035b270dbb7a1c43a473a4bfc2
digimori <hanaorikuu@gmail.com>
2023-10-18 19:29:36 +0000

    Formcontrol practice using state.

--- src/App.jsx
+++ src/App.jsx
@@ -19,16 +19,18 @@ import Clock from './components/ComponentLifecycleExample';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWU';
+import ControlledForm from './components/ControlledForm';
 
 
 function App() {
 
   return (
       <div className={css.App}>
-        <NavBarForm />
+        <ControlledForm />
+        {/*<NavBarForm />
         <Sidebar />
        <Content />
-        {/*<LifeCyclesCWU />
+        <LifeCyclesCWU />
         <LifeCyclesCDU />
         <LifeCyclesCDM />
         <Clock />
